48ec8635e8870fd31c64c27b85dcbbd1ba829e85
Rafael Bessa <104053775+Rafael-Bessa@users.noreply.github.com>
2024-02-29 23:44:12 -0300

    Primeiras modelagens

---
+++ PIM/PIM/Fornecedores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIM
+{
+    class Fornecedores
+    {
+
+        private string razaoSocial;
+        private string cnpj;
+        private string email;
+        private string telefone;
+        private List<ProdutoDoFornecedor> produtos;
+
+        public string RazaoSocial { get => razaoSocial; set => razaoSocial = value; }
+        public string Cnpj { get => cnpj; set => cnpj = value; }
+        public string Email { get => email; set => email = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+        internal List<ProdutoDoFornecedor> Produtos { get => produtos; set => produtos = value; }
+
+    }
+}
